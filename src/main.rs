5803bfc234611e32b6121a26067d4d5208230435
vivax3794 <vivax3794@protonmail.com>
2022-12-01 23:42:07 +0100

    make cli nicer

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,76 @@
-use viv_script::{compile_to_exe, compile_to_ir, compile_to_obj, find_exe, run_exe, report_error};
+use viv_script::{compile_to_exe, compile_to_ir, compile_to_obj, find_exe, report_error, run_exe};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    input_file: String,
-    output_file: String,
-    #[arg(short, long)]
-    ir: bool,
     #[arg(short, long)]
     no_optimize: bool,
-    #[arg(short, long)]
-    run: bool,
+
+    #[command(subcommand)]
+    command: Command,
 }
 
-fn main() {
-    let args = Args::parse();
+#[derive(Subcommand, Debug)]
+#[command()]
+enum Command {
+    Build {
+        input_file: String,
+        output_file: String,
+    },
+    Run {
+        input_file: String,
+    },
+    Ir {
+        input_file: String,
+        output_fie: String,
+    },
+}
 
+fn ir(optimize: bool, input_file: &str, output_file: &str) {
+    let code = std::fs::read_to_string(input_file).unwrap();
+    if let Err(err) = compile_to_ir(input_file, &code, output_file, optimize) {
+        report_error(&code, err);
+    }
+}
+
+fn build(optimize: bool, input_file: &str, output_file: &str) {
     let ir_file = temp_file::empty();
     let ir_file = ir_file.path().to_str().unwrap();
 
     let obj_file = temp_file::empty();
     let obj_file = obj_file.path().to_str().unwrap();
 
-    let exe_file = args.output_file;
+    ir(optimize, input_file, ir_file);
 
-    let code = std::fs::read_to_string(&args.input_file).unwrap();
+    let llc = find_exe(vec!["llc-12", "llc"]).expect("llc binary not found");
+    let gcc = find_exe(vec!["clang", "gcc"]).expect("gcc/clang not found on system");
 
-    if args.ir {
-        if let Err(err) = compile_to_ir(&args.input_file, &code, &exe_file, !args.no_optimize) {
-            report_error(&code, err)
-        }
-    } else {
-        let llc = find_exe(vec!["llc-12", "llc"]).expect("llc binary not found");
-        let gcc = find_exe(vec!["clang", "gcc"]).expect("gcc/clang not found on system");
+    compile_to_obj(llc, ir_file, obj_file);
+    compile_to_exe(gcc, obj_file, output_file);
+}
 
-        if let Err(err) = compile_to_ir(&args.input_file, &code, ir_file, !args.no_optimize) {
-            report_error(&code, err);
-            return;
-        }
-        compile_to_obj(llc, ir_file, obj_file);
-        compile_to_exe(gcc, obj_file, &exe_file);
+fn run(optimize: bool, input_file: &str) {
+    let exe_file = temp_file::empty();
+    let exe_file = exe_file.path().to_str().unwrap();
 
-        if args.run {
-            run_exe(&exe_file);
-        }
+    build(optimize, input_file, exe_file);
+    run_exe(exe_file);
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Build {
+            input_file,
+            output_file,
+        } => build(!args.no_optimize, &input_file, &output_file),
+        Command::Run { input_file } => run(!args.no_optimize, &input_file),
+        Command::Ir {
+            input_file,
+            output_fie,
+        } => ir(!args.no_optimize, &input_file, &output_fie),
     }
 }
